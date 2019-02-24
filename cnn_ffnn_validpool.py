eabb0b37007794d0f28b8326183dcf78cc24ae4e
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2019-02-24 13:16:39 +0100

    working with average pooling

---
+++ cnn_ffnn_validpool.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Step1: load dataset
+
+# Step2: make dataset iterable
+
+# Step3: create model class
+
+# Step4: instantiate model class
+
+# Step5: instantiate loss class
+
+# Step6: instantiate optimizer class
+
+# Parameters in depth
+
+# Step7: train model
